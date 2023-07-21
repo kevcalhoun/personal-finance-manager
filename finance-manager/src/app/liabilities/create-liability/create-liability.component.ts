27b17c0233b06dee290a96e56bd7ed3096c842e1
kevcalhoun <kwcalhou@iu.edu>
2023-07-21 16:32:00 -0500

    initial commit of working liability table population and create and delete functionality

--- finance-manager/src/app/liabilities/create-liability/create-liability.component.ts
+++ finance-manager/src/app/liabilities/create-liability/create-liability.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Liability } from '../models/liabilty';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { LiabilityService } from '../liabilities.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-liability',
   templateUrl: './create-liability.component.html',
   styleUrls: ['./create-liability.component.scss']
 })
-export class CreateLiabilityComponent {
+export class CreateLiabilityComponent implements OnInit {
+  liabilities: Liability[] = [];
+  liability!: Liability;
+  createLiabilityForm!: FormGroup;
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(public dialogRef: MatDialogRef<CreateLiabilityComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Liability,
+    private liabilityService: LiabilityService) {
+
+  }
+
+  ngOnInit(): void {
+    this.createLiabilityForm = new FormGroup({
+      liabilityName: new FormControl(null),
+      liabilityType: new FormControl(null),
+      liabilityRemainingAmount: new FormControl(null),
+      liabilityInterestRate: new FormControl(null),
+      liabilityMinMonthlyPayment: new FormControl(null)
+    })
+  }
+
+  public onCreateLiability(formData: Liability): void {
+    // const createFormData: Liability = this.createLiabilityForm.value;
+    console.log(formData);
+    this.liabilityService.createLiability(formData).subscribe({
+      next: (response: Liability) => {
+        console.log(response);
+        this.getLiabilities();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+        this.createLiabilityForm.reset()
+      }, 
+      complete: () => {
+        console.log(this.liabilities);
+        this.createLiabilityForm.reset();
+      }
+    });
+  }
+
+  public getLiabilities(): void {
+    this.liabilities = [];
+    this.liabilityService.getLiabilities().subscribe({
+      next: data => {
+        this.liabilities = data;
+      }, 
+      error: (err: HttpErrorResponse) => {
+        alert(err.message);
+      },
+      complete: () => {
+        console.log("all liabilities loaded after created new")
+      }
+    });
+  }
 
 }
--- finance-manager/src/app/liabilities/liabilities.component.ts
+++ finance-manager/src/app/liabilities/liabilities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Liability } from './models/liabilty';
 
 @Component({
   selector: 'app-liabilities',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiabilitiesComponent implements OnInit {
   title = "Liabilites";
+  liabilities: Liability[] = [];
   constructor() { }
 
   ngOnInit(): void {
