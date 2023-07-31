02a119fac57e7a98ead512037862ed77256826e5
kevcalhoun <kwcalhou@iu.edu>
2023-07-31 14:04:23 -0500

    calculation clean up

--- finance-manager/src/app/dashboard/dashboard.component.ts
+++ finance-manager/src/app/dashboard/dashboard.component.ts
@@ -5,6 +5,8 @@ import { Asset } from '../assets/models/asset';
 import { DashboardService } from './dashboard.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { AssetService } from '../assets/assets.service';
+import { Liability } from '../liabilities/models/liabilty';
+import { LiabilityService } from '../liabilities/liabilities.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,18 +22,25 @@ export class DashboardComponent implements OnInit {
     hideRequired: this.hideRequiredControl,
     floatLabel: this.floatLabelControl,
   });
-
+  // Asset Info Properties
   assets: Asset[] = [];
   totalAssetAmount!: number;
   goalProgressAverage!: number;
+
+  //Liability Info Properties
+  liabilities: Liability[] = [];
+  totalLiabilityAmount!: number;
   
   constructor(private _formBuilder: FormBuilder,
-    private assetService: AssetService) { }
+    private assetService: AssetService,
+    private liabilityService: LiabilityService) { }
 
   ngOnInit() {
     this.getAssetDetails();
+    this.getLiabilityDetails();
   }
 
+  // TODO: Merge the following methods and pass values into new method to get net worth
   getAssetDetails(): void {
     this.assets = [];
     this.assetService.getAssets().subscribe({
@@ -48,6 +57,28 @@ export class DashboardComponent implements OnInit {
         this.totalAssetAmount = currentAssetsTotal;
         this.goalProgressAverage = currentGoalProgressAvg;
         console.log(this.totalAssetAmount);
+      }, 
+      error: (err: HttpErrorResponse) => {
+        alert(err.message);
+      },
+      complete: () => {
+      }
+    });
+  }  
+
+  getLiabilityDetails(): void {
+    this.liabilities = [];
+    this.liabilityService.getLiabilities().subscribe({
+      next: data => {
+        data.forEach(element => {
+          this.liabilities.push(element);
+        });
+        console.log(this.liabilities);
+        
+        const currentLiabilitiesTotal: number = this.liabilities.map(a => a.liabilityRemainingAmount).reduce(function(a, b) {
+          return a + b;
+        });
+        this.totalLiabilityAmount = currentLiabilitiesTotal;
       }, 
       error: (err: HttpErrorResponse) => {
         alert(err.message);
